7dc2031262fcb96a0baeca2042034a433062f607
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2022-05-02 02:23:28 +0300

    10_Soruluk_Yarisma

---
+++ Bilgi_Yarismasi/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Bilgi_Yarismasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.soruLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonA = new System.Windows.Forms.Button();
+            this.buttonB = new System.Windows.Forms.Button();
+            this.buttonD = new System.Windows.Forms.Button();
+            this.buttonC = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.trueBox = new System.Windows.Forms.PictureBox();
+            this.falseBox = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trueBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.falseBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.soruLabel);
+            this.panel1.Location = new System.Drawing.Point(7, 150);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(559, 131);
+            this.panel1.TabIndex = 0;
+            // 
+            // soruLabel
+            // 
+            this.soruLabel.Location = new System.Drawing.Point(19, 12);
+            this.soruLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.soruLabel.Name = "soruLabel";
+            this.soruLabel.Size = new System.Drawing.Size(536, 105);
+            this.soruLabel.TabIndex = 0;
+            this.soruLabel.Text = "Bilgi Yarışmasına Hoşgeldiniz\r\nBaşarılar\r\n";
+            this.soruLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(3, 12);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(864, 122);
+            this.panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Viner Hand ITC", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(326, 44);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 48);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bilgi Yarışması";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(638, 3);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(223, 122);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(4, 3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 115);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonA
+            // 
+            this.buttonA.Location = new System.Drawing.Point(47, 299);
+            this.buttonA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonA.Name = "buttonA";
+            this.buttonA.Size = new System.Drawing.Size(169, 35);
+            this.buttonA.TabIndex = 2;
+            this.buttonA.Text = "A)";
+            this.buttonA.UseVisualStyleBackColor = true;
+            this.buttonA.Visible = false;
+            this.buttonA.Click += new System.EventHandler(this.buttonA_Click);
+            // 
+            // buttonB
+            // 
+            this.buttonB.Location = new System.Drawing.Point(329, 299);
+            this.buttonB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonB.Name = "buttonB";
+            this.buttonB.Size = new System.Drawing.Size(185, 35);
+            this.buttonB.TabIndex = 3;
+            this.buttonB.Text = "B)";
+            this.buttonB.UseVisualStyleBackColor = true;
+            this.buttonB.Visible = false;
+            this.buttonB.Click += new System.EventHandler(this.buttonB_Click);
+            // 
+            // buttonD
+            // 
+            this.buttonD.Location = new System.Drawing.Point(329, 351);
+            this.buttonD.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonD.Name = "buttonD";
+            this.buttonD.Size = new System.Drawing.Size(185, 41);
+            this.buttonD.TabIndex = 5;
+            this.buttonD.Text = "D)";
+            this.buttonD.UseVisualStyleBackColor = true;
+            this.buttonD.Visible = false;
+            this.buttonD.Click += new System.EventHandler(this.buttonD_Click);
+            // 
+            // buttonC
+            // 
+            this.buttonC.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonC.Location = new System.Drawing.Point(47, 352);
+            this.buttonC.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonC.Name = "buttonC";
+            this.buttonC.Size = new System.Drawing.Size(169, 40);
+            this.buttonC.TabIndex = 4;
+            this.buttonC.Text = "C)";
+            this.buttonC.UseVisualStyleBackColor = true;
+            this.buttonC.Visible = false;
+            this.buttonC.Click += new System.EventHandler(this.buttonC_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(662, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Soru No :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(618, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Doğru Sayısı :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(611, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 21);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Yanlış Sayısı :";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(651, 274);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(157, 32);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Sonraki Soru";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(789, 234);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 21);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(789, 191);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 21);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(789, 150);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 21);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "0";
+            // 
+            // trueBox
+            // 
+            this.trueBox.Image = ((System.Drawing.Image)(resources.GetObject("trueBox.Image")));
+            this.trueBox.Location = new System.Drawing.Point(583, 327);
+            this.trueBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.trueBox.Name = "trueBox";
+            this.trueBox.Size = new System.Drawing.Size(115, 48);
+            this.trueBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.trueBox.TabIndex = 13;
+            this.trueBox.TabStop = false;
+            this.trueBox.Visible = false;
+            // 
+            // falseBox
+            // 
+            this.falseBox.Image = ((System.Drawing.Image)(resources.GetObject("falseBox.Image")));
+            this.falseBox.Location = new System.Drawing.Point(743, 327);
+            this.falseBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.falseBox.Name = "falseBox";
+            this.falseBox.Size = new System.Drawing.Size(115, 48);
+            this.falseBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.falseBox.TabIndex = 14;
+            this.falseBox.TabStop = false;
+            this.falseBox.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(869, 403);
+            this.Controls.Add(this.falseBox);
+            this.Controls.Add(this.trueBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonD);
+            this.Controls.Add(this.buttonC);
+            this.Controls.Add(this.buttonB);
+            this.Controls.Add(this.buttonA);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Bilgi Yarışması";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trueBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.falseBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label soruLabel;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private PictureBox pictureBox2;
+        private Button buttonA;
+        private Button buttonB;
+        private Button buttonD;
+        private Button buttonC;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private PictureBox trueBox;
+        private PictureBox falseBox;
+    }
+}
---
+++ Bilgi_Yarismasi/Form1.cs
@@ -0,0 +1,335 @@
+﻿namespace Bilgi_Yarismasi
+{
+    public partial class Form1 : Form
+    {
+
+        int soruNo = 0, dogru, yanlis;
+
+        //butona basilip basilmadigini tutacagimiz degisken
+        int tiklanildi = 0;
+
+        String dogruCevap = "";
+
+        //tiklanip tiklanmadigini atayacagimiz diger degisken ve ana metodumuz olan button5_Click'te kullaniyoruz
+        int girdi = 0;
+
+        
+
+        private void buttonA_Click(object sender, EventArgs e)
+        {
+            if(dogruCevap == buttonA.Text)
+            {
+                //dogru oldugunda green light gif yanacak
+                trueBox.Visible = true;
+                //dogru sayisini bir artirip label'e atiyoruz
+                dogru++;
+                label7.Text = Convert.ToString( dogru);
+                //herhangi bir butona tiklanildigini tutan degiskenimiz
+                tiklanildi++;
+            }
+            else
+            {
+                //yanlis oldugunda red light gif yanacak
+                falseBox.Visible = true;
+                //yanlis sayisini bir artirip label'e atiyoruz
+                yanlis++;
+                label6.Text = Convert.ToString( yanlis);
+                //herhangi bir butona tiklanildigini tutan degiskenimiz
+                tiklanildi++;
+            }
+            //buton'a bir daha tiklanma ozelligini kapatiyoruz
+            butonFalseEnableMetodu();
+
+            //diger butonlarda da ayni ozellikleri kullandik
+        }
+
+        private void buttonB_Click(object sender, EventArgs e)
+        {
+            if (dogruCevap == buttonB.Text)
+            {
+                trueBox.Visible = true;
+                dogru++;
+                label7.Text = Convert.ToString(dogru);
+                tiklanildi++;
+            }
+            else
+            {
+                falseBox.Visible = true;
+                yanlis++;
+                label6.Text = Convert.ToString(yanlis);
+                tiklanildi++;
+            }
+            butonFalseEnableMetodu();
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            if (dogruCevap == buttonC.Text)
+            {
+                trueBox.Visible = true;
+
+                dogru++;
+                label7.Text = Convert.ToString(dogru);
+                tiklanildi++;
+            }
+            else
+            {
+                falseBox.Visible = true;
+                yanlis++;
+                label6.Text = Convert.ToString(yanlis);
+                tiklanildi++;
+            }
+            butonFalseEnableMetodu();
+        }
+        private void buttonD_Click(object sender, EventArgs e)
+        {
+            if (dogruCevap == buttonD.Text)
+            {
+                trueBox.Visible = true;
+
+                dogru++;
+                label7.Text = Convert.ToString(dogru);
+                tiklanildi++;
+            }
+            else
+            {
+                falseBox.Visible = true;
+                yanlis++;
+                label6.Text = Convert.ToString(yanlis);
+                tiklanildi++;
+            }
+            butonFalseEnableMetodu();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //form'umuza Restart ozelligi atadik ve text'teki yazimizi kontrol ettik
+            if(button5.Text == "Yeniden Başlat")
+            {
+               Application.Restart();
+            }
+            else
+            {
+                //soruNo diye bir degiskenimiz var,hangi soruda oldugumuzu tutuyor,default degeri de '0' oldugundan
+                //bir artirip devam ediyoruz 
+                if (soruNo == 0)
+                {
+                    soruNo++;
+
+                    //converter
+                    label8.Text = soruNo.ToString();
+
+                    //sorular() metodumuzda 'sorularımızı' donduruyoruz ve parametre olarak 'soruNo' degiskenimizi atiyoruz
+                    sorular(soruNo);
+                }
+                //1.sorudan itibaren girilecek kosul sarti blogu
+                else if (soruNo != 0)
+                {
+                    //hicbir butonun secilip secilmedigini kontrol eden metodumuzu calistiriyoruz
+                    tiklanmaKontrol();
+                    //eger hicbir buton tiklanmadiysa 'girdi' degerimizin default degeri '0' olur ve ileri soruya gidemeyiz
+                    if (girdi != 0)
+                    {
+                        //ileri soruya gidemedigimizden dolayi 'soruNo' degiskenimiz artmadan kalir
+                        label8.Text = soruNo.ToString();
+                        //tekrar ayni soruyu dondururuz
+                        sorular(soruNo);
+                        //ve her defasinda girdimizi sifirlayip yeniden kontrol edilmeye hazir hale getiririz
+                        girdi = 0;
+                    }
+                    else
+                    {
+                        //herhangi bir buton secildiyse girilecek sart blogu
+                        //'soruNo' bir artirilip sonraki soruya gecilir
+                        soruNo++;
+
+                        label8.Text = soruNo.ToString();
+
+                        sorular(soruNo);
+
+
+
+                    }
+                }
+            }
+            
+            
+        }
+        //butonlari erisilebilir hale getiriyoruz
+        public void butonTrueEnableMetodu()
+        {
+            buttonA.Enabled = true;
+            buttonB.Enabled = true;
+            buttonC.Enabled = true;
+            buttonD.Enabled = true;
+        }
+        //butonlari erisilemez hale getiriyoruz
+        public void butonFalseEnableMetodu()
+        {
+            buttonA.Enabled = false;
+            buttonB.Enabled = false;
+            buttonC.Enabled = false;
+            buttonD.Enabled = false;
+        }
+
+        //herhangi bir butonun tiklanip tiklanmadigini kontrol edecek metod
+        public void tiklanmaKontrol()
+        {
+            //bir butona tiklanilmadiysa hata mesajini ekrana vericez
+            if(tiklanildi == 0)
+            {
+                MessageBox.Show("\n Hiçbir Cevap Seçilmedi. \n Diğer Soruya Geçemezsiniz !!! \n Lütfen Cevap Seçiniz !!!");
+                girdi++;
+            }
+        }
+        //sorularimizi dondurecegimiz metod
+        public void sorular(int soruNo)
+        {
+            if (soruNo == 1)
+            {
+                
+                soruLabel.Text = "Cumhuriyet Kaç Yılında Kurulmuştur ? ";
+                buttonA.Text = "1920";
+                buttonB.Text = "1921";
+                buttonC.Text = "1922";
+                buttonD.Text = "1923";
+                dogruCevap = "1923";
+                //butonlari default olarak 'invisible' yapmistik
+                //ilk sorudan itibaren 'visible' yaptik
+                buttonA.Visible = true;buttonB.Visible = true;buttonC.Visible = true;buttonD.Visible = true;
+
+            }
+            else if (soruNo == 2)
+            {
+                //true-false giflerimizi her sorudan sonra tekrardan invisible yapiyoruz
+                trueBox.Visible = false; falseBox.Visible = false;
+                //butonlara erisim sagliyoruz
+                butonTrueEnableMetodu();
+                tiklanildi = 0;
+                soruLabel.Text = "Yumurta ve irmikle yapılan,fırında piştikten sonra üzerine şerbet " +
+                    "dökülen tatlının adı nedir ? ";
+                buttonA.Text = "Lokma";
+                buttonB.Text = "Revani";
+                buttonC.Text = "Şekerpare";
+                buttonD.Text = "Kemalpaşa";
+                dogruCevap = "Revani";
+            }
+            else if (soruNo == 3)
+            {
+                trueBox.Visible = false; falseBox.Visible = false;
+                butonTrueEnableMetodu();
+                tiklanildi = 0;
+                soruLabel.Text = "Eline geçeni zamansız ve ayırt etmeden yiyen kişiye ne denir? ";
+                buttonA.Text = "Açık Ağız";
+                buttonB.Text = "Pis Boğaz";
+                buttonC.Text = "Tıknaz";
+                buttonD.Text = "Savurgan";
+                dogruCevap = "Pis Boğaz";
+            }
+            else if (soruNo == 4)
+            {
+                trueBox.Visible = false; falseBox.Visible = false;
+                butonTrueEnableMetodu();
+                tiklanildi = 0;
+                soruLabel.Text = "Romen Rakamında Hangi Sayı Yoktur? ";
+                buttonA.Text = "0";
+                buttonB.Text = "50";
+                buttonC.Text = "100";
+                buttonD.Text = "1000";
+                dogruCevap = "0";
+            }
+            else if (soruNo == 5)
+            {
+                trueBox.Visible = false; falseBox.Visible = false;
+                butonTrueEnableMetodu();
+                tiklanildi = 0;
+                soruLabel.Text = "Magna Carta hangi ülkenin kralıyla yapılmış bir sözleşmedir? ";
+                buttonA.Text = "İspanya";
+                buttonB.Text = "Fransa";
+                buttonC.Text = "İngiltere";
+                buttonD.Text = "Almanya";
+                dogruCevap = "İngiltere";
+            }
+            else if (soruNo == 6)
+            {
+                trueBox.Visible = false; falseBox.Visible = false;
+                butonTrueEnableMetodu();
+                tiklanildi = 0;
+                soruLabel.Text = "Galatasaray hangi yıl UEFA kupasını almıştır? ";
+                buttonA.Text = "2000";
+                buttonB.Text = "2001";
+                buttonC.Text = "2002";
+                buttonD.Text = "2003";
+                dogruCevap = "2000";
+            }
+            else if (soruNo == 7)
+            {
+                trueBox.Visible = false; falseBox.Visible = false;
+                butonTrueEnableMetodu();
+                tiklanildi = 0;
+                soruLabel.Text = "Kıbrıs Barış harekatı hangi tarihte gerçekleşmiştir? ";
+                buttonA.Text = "1970";
+                buttonB.Text = "1972";
+                buttonC.Text = "1974";
+                buttonD.Text = "1976";
+                dogruCevap = "1974";
+            }
+            else if (soruNo == 8)
+            {
+                trueBox.Visible = false; falseBox.Visible = false;
+                butonTrueEnableMetodu();
+                tiklanildi = 0;
+                soruLabel.Text = "Osmanlı’da Lale devri hangi padişah döneminde yaşamıştır? ";
+                buttonA.Text = "3.Ahmet";
+                buttonB.Text = "4.Murat";
+                buttonC.Text = "3.Selim";
+                buttonD.Text = "4.Mehmet";
+                dogruCevap = "3.Ahmet";
+            }
+            else if (soruNo == 9)
+            {
+                trueBox.Visible = false; falseBox.Visible = false;
+                butonTrueEnableMetodu();
+                tiklanildi = 0;
+                soruLabel.Text = "Bir elektrik devresinde direnç hangi harfle gösterilir? ";
+                buttonA.Text = "D";
+                buttonB.Text = "R";
+                buttonC.Text = "A";
+                buttonD.Text = "G";
+                dogruCevap = "R";
+            }
+            else if (soruNo == 10)
+            {
+                trueBox.Visible = false; falseBox.Visible = false;
+                butonTrueEnableMetodu();
+                tiklanildi = 0;
+                soruLabel.Text = "İstiklal Marşı hangi yıl yazılmıştır? ";
+                buttonA.Text = "1919";
+                buttonB.Text = "1920";
+                buttonC.Text = "1921";
+                buttonD.Text = "1922";
+                dogruCevap = "1921";
+
+                button5.Text = "Bitir";
+            }
+            
+            else
+            {
+                //sorularimiz bittiginde 'soruNo' degiskenimizin artmasini onlemek icin bu sarta giriyoruz
+                label8.Text = "10";
+                
+                MessageBox.Show("\n Bütün Soruları Cevapladınız \n \n Toplam Soru Sayısı : 10 " +
+                    "\n Doğru Sayınız : " + dogru + "\n Yanlış Sayınız : " + yanlis + "\n Doğru Yüzdeniz : % " + (100 * dogru)/10);
+                button5.Text = "Yeniden Başlat";
+                //MessageBox'tan sonra kullaniciya 'yeniden baslayabilme' sansini veriyoruz
+
+            }
+
+        }
+    }
+}
